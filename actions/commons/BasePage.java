26623cf7464e4906f7d9bd10b80a615e1236134b
Phat Le <phatl2511@gmail.com>
2022-05-01 11:19:32 +0700

    framework_Topic 49

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,5 +1,310 @@
 package commons;
 
-public class BasePage {
+import java.util.List;
+import java.util.Set;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.Color;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+	
+	
+	public static BasePage getBasePageInstance() {
+		return new BasePage();
+	}
+	
+	public void switchToWindowByID(WebDriver driver, String expectedID) {
+		Set<String> allTabIDs = driver.getWindowHandles();
+		
+		for (String id : allTabIDs) {
+			if (!id.equals(expectedID)) {
+				driver.switchTo().window(id);
+				break;
+			}
+		}
+	}
+
+	public void getTabTitle(WebDriver driver, String expectedTitle) {
+		Set<String> allTabTitle = driver.getWindowHandles();
+		
+		for (String tabTitle : allTabTitle) {
+			driver.switchTo().window(tabTitle);
+			
+			String actualTitle = driver.getTitle();
+			
+			if (actualTitle.equals(expectedTitle)) {	
+				break;
+			}
+		}
+		
+	}
+	
+	public boolean closeAllWindowsWithoutParent(WebDriver driver, String ParentID) {
+
+		Set<String> allWindows = driver.getWindowHandles();
+		
+		for (String runWindows : allWindows) {
+			if (!runWindows.equals(ParentID)) {
+				driver.switchTo().window(runWindows);
+				driver.close();
+			}
+		}
+		driver.switchTo().window(ParentID);
+		if(driver.getWindowHandles().size() == 1)
+			return true;
+		else 
+			return false;
+	}
+	
+	public void openPageUrl(WebDriver driver, String pageUrl) {
+		driver.get(pageUrl);
+	}
+	
+	public String getPageUrl(WebDriver driver) {
+		return driver.getCurrentUrl();
+	}
+	
+	public String getPageTitle(WebDriver driver) {
+		return driver.getTitle();
+	}
+	
+	public String getPageSource(WebDriver driver) {
+		return driver.getPageSource();
+	}
+	
+	public void backToPage(WebDriver driver) {
+		driver.navigate().back();
+	}
+	
+	public void forwardToPage(WebDriver driver) {
+		driver.navigate().forward();
+	}
+	
+	public void refreshCurrentPage(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+	
+	public void acceptAlert(WebDriver driver) {
+		waitForAlertPresence(driver).accept();
+	}
+	
+	public void cancelAlert(WebDriver driver) {
+		waitForAlertPresence(driver).dismiss(); 
+	}
+	
+	public void sendKeysToAlert(WebDriver driver, String valuetoSendKey) {
+		waitForAlertPresence(driver) .sendKeys(valuetoSendKey); 
+	}
+ 	
+	public String getAlertText(WebDriver driver) {
+		 return driver.switchTo().alert().getText();
+	}
+	
+	public By getByXpath(String locator) {
+		return By.xpath(locator);
+	}
+	
+	public WebElement getElement(WebDriver driver, String locator) {
+		return driver.findElement(getByXpath(locator));
+	}
+	
+	public List<WebElement> getElements(WebDriver driver, String locator) {
+		return driver.findElements(getByXpath(locator));
+	}
+	
+	public void clickToElement(WebDriver driver, String locator) {
+		getElement(driver, locator).click();
+	}
+	
+	public void sendKeyToElement(WebDriver driver, String locator, String keyToSend) {
+		WebElement  element = getElement(driver, locator);
+		element.clear();
+		element.sendKeys(keyToSend);
+	}
+	
+	public String getElementText(WebDriver driver, String locator) {
+		return getElement(driver, locator).getText();
+	}
+	
+	public void selectInDefaultDropDown(WebDriver driver, String locator, String  itemText) {
+		Select select = new Select(getElement(driver, locator));
+		select.selectByVisibleText(itemText);
+	}
+	
+	public String getFirstSelectedItem(WebDriver driver, String locator) {
+		Select select = new Select(getElement(driver, locator));
+		return select.getFirstSelectedOption().getText();
+	}
+	
+	public boolean isMultipleDropDown(WebDriver driver, String locator) {
+		Select select = new Select(getElement(driver, locator));
+		return select.isMultiple();
+	}
+	
+	public void selectInCustomDropDownList(WebDriver driver, String parentLocator, String childLocator, String expectedText) {
+		getElement(driver, parentLocator).click();
+		
+		List<WebElement> allNumbers = new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(childLocator)));
+		
+		for (WebElement number : allNumbers) {
+			String actualText = number.getText();
+			
+			if (actualText.equals(expectedText)) {
+				
+				((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", number);
+				
+				sleepInSecond(3);
+				
+				number.click();
+				
+				sleepInSecond(3);
+		
+				break;
+			}
+		}
+	}
+	
+	public String getElementAttribute(WebDriver driver, String Locator, String attributeName) {
+		return getElement(driver, Locator).getAttribute(attributeName); 
+	}
+	
+	public String getElementCssValue(WebDriver driver, String Locator, String propertyName) {
+		return getElement(driver, Locator).getCssValue(propertyName);
+	}
+	
+	public int getListElementSize(WebDriver driver, String Locator) {
+		return getElements(driver, Locator).size();
+	}
+	
+	public String getHexaColorByRgbaColor(String rgbaValue) {
+		return Color.fromString(rgbaValue).asHex();
+	}
+	 
+	public void checkToCheckBoxOrRadio(WebDriver driver, String Locator) {
+		WebElement element = getElement(driver, Locator);
+		if (!element.isSelected()){
+			element.click();
+		}
+	}
+	
+	public void unCheckToCheckBox(WebDriver driver, String Locator) {
+		WebElement element = getElement(driver, Locator);
+		if (element.isSelected()) {
+			element.click();
+		} 
+	}
+  	
+	public boolean isElementDisplayed(WebDriver driver, String Locator) {
+		return getElement(driver, Locator).isDisplayed();
+	}
+	
+	public boolean isElementSelected(WebDriver driver, String Locator) {
+		return getElement(driver, Locator).isSelected();
+	}
+	
+	public boolean isElementEnabled(WebDriver driver, String Locator) {
+		return getElement(driver, Locator).isEnabled();
+	} 
+	
+	public void switchToIframe(WebDriver driver, String Locator) {
+		driver.switchTo().frame(getElement(driver, Locator));
+	}
+	
+	public void switchToDefaultContent(WebDriver driver) {
+		driver.switchTo().defaultContent();
+	}
+	
+	public void doubleClickToElement(WebDriver driver, String Locator) {
+		Actions action = new Actions(driver);
+		action.doubleClick(getElement(driver, Locator)).perform();
+	}
+	
+	public void hoverToElement(WebDriver driver, String Locator) {
+		Actions action = new Actions(driver);
+		action.moveToElement(getElement(driver, Locator)).perform();
+	}
+	
+	public void rightClickToElement(WebDriver driver, String Locator) {
+		Actions action = new Actions(driver);
+		action.contextClick(getElement(driver, Locator)).perform();
+	}
+	
+	public void dragAndDropToElement(WebDriver driver, String sourceLocator, String targetLocator) {
+		Actions action = new Actions(driver);
+		action.dragAndDrop(getElement(driver, sourceLocator), getElement(driver, targetLocator)).perform();
+	}
+	
+	public void sendKeysToElement(WebDriver driver, String Locator, Keys key) {
+		Actions action = new Actions(driver);
+		action.sendKeys(getElement(driver, Locator), key).perform(); 
+	}
+	
+	public void clickToElementByJS(WebDriver driver, String locator) {
+		((JavascriptExecutor) driver).executeScript("arguments[0].click();", getElement(driver, locator));
+	}
+
+	public void scrollToElementOnTop(WebDriver driver, String locator) {
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", getElement(driver, locator));
+	}
+
+	public void scrollToElementOnDown(WebDriver driver, String locator) {
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(false);", getElement(driver, locator));
+	}
+
+	public void sendkeyToElementByJS(WebDriver driver, String locator, String value) {
+		((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute('value', '" + value + "')", getElement(driver, locator));
+	}
+
+	public void removeAttributeInDOM(WebDriver driver, String locator, String attributeRemove) {
+		((JavascriptExecutor) driver).executeScript("arguments[0].removeAttribute('" + attributeRemove + "');", getElement(driver, locator));
+	}
+	
+	public void hightlightElement(WebDriver driver, String locator) {
+		WebElement element = getElement(driver, locator);
+		String originalStyle = element.getAttribute("style");
+		((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute('style', arguments[1])", element, "border: 2px solid red; border-style: dashed;");
+		sleepInSecond(1);
+		((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute('style', arguments[1])", element, originalStyle);
+	}
+	
+	public void waitForElementVisible(WebDriver driver, String locator) {
+		new WebDriverWait(driver, longTimeout).until(ExpectedConditions.visibilityOfElementLocated(getByXpath(locator)));
+	}
+	
+	public void waitForElementPresence(WebDriver driver, String locator) {
+		new WebDriverWait(driver, longTimeout).until(ExpectedConditions.presenceOfElementLocated(getByXpath(locator)));
+	} 
+	
+	public boolean waitForElementInvisible(WebDriver driver, String locator) {
+		return new WebDriverWait(driver, longTimeout).until(ExpectedConditions.invisibilityOfElementLocated(getByXpath(locator)));
+	}
+	
+	public void waitForElementClickable(WebDriver driver, String locator) {
+		new WebDriverWait(driver, longTimeout).until(ExpectedConditions.elementToBeClickable(getByXpath(locator)));
+	}
+ 	
+	public Alert waitForAlertPresence(WebDriver driver) {
+		return new WebDriverWait(driver, longTimeout).until(ExpectedConditions.alertIsPresent());
+	}
+	
+	public void sleepInSecond(long second) {
+		try {
+			Thread.sleep(second * 3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private long longTimeout = 30;
+		
 }
---
+++ testcases/com/techpanda/account/Level_2_Apply_BasePage_Part_I.java
@@ -0,0 +1,103 @@
+package com.techpanda.account;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import commons.BasePage;
+
+public class Level_2_Apply_BasePage_Part_I {
+	
+	WebDriver driver;
+	BasePage basePage;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+	
+  
+  @BeforeClass
+  public void beforeClass() {
+	  if(osName.toUpperCase().startsWith("MAC")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/BrowserDrivers/geckodriver");
+			System.setProperty("webdriver.chrome.driver", projectPath + "/BrowserDrivers/chromedriver");
+		}else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\BrowserDrivers\\geckodriver.exe");
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\BrowserDrivers\\chromedriver.exe");
+		}
+		
+		driver = new ChromeDriver();
+		basePage = new BasePage();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();  
+  }
+  
+  	@BeforeMethod
+  	public void BeforeMethod() {
+  		basePage.openPageUrl(driver, "http://live.techpanda.org");
+  	}
+  
+  
+  	@Test
+	public void TC_01_Login_Empty_Data() {
+  		basePage.clickToElement(driver, "//div[@class='footer-container']//a[text()='My Account']");
+  		basePage.sendKeyToElement(driver, "//div[@class='col-2 registered-users']//input[@type='email']", "");
+  		basePage.sendKeyToElement(driver, "//div[@class='col-2 registered-users']//input[@type='password']", "");
+  		basePage.clickToElement(driver, "//button[@title='Login']");
+		
+  		Assert.assertEquals(basePage.getElementText(driver, "//div[@id='advice-required-entry-email']"), "This is a required field.");
+		Assert.assertEquals(basePage.getElementText(driver, "//div[@id='advice-required-entry-pass']"), "This is a required field.");
+	}
+	
+	@Test
+	public void TC_02_Invalid_Email() {
+		
+		basePage.clickToElement(driver, "//div[@class='footer-container']//a[text()='My Account']");
+  		basePage.sendKeyToElement(driver, "//div[@class='col-2 registered-users']//input[@type='email']", "thanhphat@1241");
+  		basePage.sendKeyToElement(driver, "//div[@class='col-2 registered-users']//input[@type='password']", "123456");
+  		basePage.clickToElement(driver, "//button[@title='Login']");
+		Assert.assertEquals(basePage.getElementText(driver,"//div[@id='advice-validate-email-email']"), "Please enter a valid email address. For example johndoe@domain.com.");
+	}
+	
+	@Test
+	public void TC_03_Invalid_Password() {
+		
+		basePage.clickToElement(driver, "//div[@class='footer-container']//a[text()='My Account']");
+  		basePage.sendKeyToElement(driver, "//div[@class='col-2 registered-users']//input[@type='email']", "thanhphat@gmail.com");
+  		basePage.sendKeyToElement(driver, "//div[@class='col-2 registered-users']//input[@type='password']", "123");
+  		basePage.clickToElement(driver, "//button[@title='Login']");
+		
+		Assert.assertEquals(basePage.getElementText(driver,"//div[@id='advice-validate-password-pass']"), "Please enter 6 or more characters without leading or trailing spaces.");
+	}
+	
+	@Test
+	public void TC_04_Incorrect_Email_Password() {
+		
+		basePage.clickToElement(driver, "//div[@class='footer-container']//a[text()='My Account']");
+  		basePage.sendKeyToElement(driver, "//div[@class='col-2 registered-users']//input[@type='email']", "thanhphat@gmail.com");
+  		basePage.sendKeyToElement(driver, "//div[@class='col-2 registered-users']//input[@type='password']", "123123123123");
+  		basePage.clickToElement(driver, "//button[@title='Login']");
+		
+		Assert.assertEquals(basePage.getElementText(driver, "//li[@class='error-msg']//span"), "Invalid login or password.");
+	}
+  
+  
+  
+  
+  
+  public int getRandomNumber() {
+		Random rand = new Random();
+		return rand.nextInt(999);
+	}
+  
+
+  @AfterClass
+  public void afterClass() {
+	   driver.quit();
+  }
+
+}
